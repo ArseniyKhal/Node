448d13903a1b224e700b33669d6bab66e5a61f63
ArseniyKhal <asmn@mail.ru>
2023-10-27 10:54:07 +0500

    ответ на параметр users

--- src/app.js
+++ src/app.js
@@ -1,20 +1,42 @@
 const http = require("http");
 const getUsers = require("./modules/users");
 
-const server = http.createServer((request, response) => {
-  if (request.url === "/users") {
-    response.status = 200;
-    response.statusMessage = "OK";
-    response.header = "Content-Type: application/json";
-    response.write(getUsers());
-    response.end();
+const server = http.createServer((req, res) => {
+  const url = new URL(req.url, `http://${req.headers.host}`);
+  //   const searchParams = new URLSearchParams(window.location.search);
+  //   console.log(searchParams);
+  const nameParam = url.searchParams.get("hello");
+  const userf = url.searchParams.get("users");
+  //   console.log(userf);
+  //   if (req.url === "/users") {
+  //     res.status = 200;
+  //     res.statusMessage = "OK";
+  //     res.header = "Content-Type: application/json";
+  //     res.write(getUsers());
+  //     res.end();
+  //     return;
+  //   }
+  if (url.searchParams.has("users")) {
+    res.status = 200;
+    res.statusMessage = "OK";
+    res.header = "Content-Type: application/json";
+    res.write(getUsers());
+    res.end();
     return;
   }
-  response.status = 200;
-  response.statusMessage = "OK";
-  response.header = "Content-Type: text/plain";
-  response.write("Hello, World!");
-  response.end();
+  if (url.searchParams.get("hello")) {
+    res.status = 200;
+    res.statusMessage = "OK";
+    res.header = "Content-Type: text/plain";
+    res.write(`Hello, ${nameParam}.`);
+    res.end();
+    return;
+  }
+  res.status = 200;
+  res.statusMessage = "OK";
+  res.header = "Content-Type: text/plain";
+  res.write("Hello, World!");
+  res.end();
 });
 
 server.listen(3003, () => {
