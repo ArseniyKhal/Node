72a780d7dd79d30088efc4cfe72249d14b14824a
ArseniyKhal <92600602+ArseniyKhal@users.noreply.github.com>
2023-11-03 17:58:45 +0500

    Revert "Hw 6.2"

--- src/app.js
+++ src/app.js
@@ -1,46 +1,22 @@
-const express = require("express");
-const app = express();
-const dotenv = require("dotenv");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const userRouter = require("./routes/users");
-const bodyParser = require("body-parser");
-const loggerOne = require("./middlewares/loggerOne");
+const http = require("http");
+const getUsers = require("./modules/users");
 
-dotenv.config();
-app.use(userRouter);
-
-const {
-  PORT = 3000,
-  API_URL = "http://127.0.0.1",
-  MONGO_URL = "mongodb://127.0.0.1:27017/mydb",
-} = process.env;
-
-mongoose.connect(MONGO_URL);
-const database = mongoose.connection;
-database.on("error", (error) => {
-  console.log(error);
-});
-database.once("connected", () => {
-  console.log("Connected to MongoDB");
+const server = http.createServer((request, response) => {
+  if (request.url === "/users") {
+    response.status = 200;
+    response.statusMessage = "OK";
+    response.header = "Content-Type: application/json";
+    response.write(getUsers());
+    response.end();
+    return;
+  }
+  response.status = 200;
+  response.statusMessage = "OK";
+  response.header = "Content-Type: text/plain";
+  response.write("Hello, World!");
+  response.end();
 });
 
-const helloWorld = (request, response) => {
-  response.status(200);
-  response.send("hello, World!");
-};
-
-app.get("/", helloWorld);
-
-app.use(cors());
-app.use(bodyParser.json());
-app.use(loggerOne);
-
-app.post("/", (request, response) => {
-  response.status(200);
-  response.send("Hello from POST");
-});
-
-app.listen(PORT, () => {
-  console.log(`Сервер запущен по адресу ${API_URL}:${PORT}`);
+server.listen(3003, () => {
+  console.log("Сервер запущен по адресу http://127.0.0.1:3003");
 });
